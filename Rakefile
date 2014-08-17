10ce6a8e6f43725e92eec42261d26a6b7426b267
Ignacio Duarte <iduarte@thoughtworks.com>
2014-08-17 12:04:43 +0100

    @3oheme refined rakefile

--- Rakefile
+++ Rakefile
@@ -1,13 +1,19 @@
-task default: %w[runapp]
+task default: ['render_html']
 
-task :runapp do
+task :render_html do
+  puts 'Rendering html file...'
   ruby 'app.rb'
 end
 
 task :push do
-  ruby 'app.rb'
-  `git checkout gh-pages`
-  `git merge master`
-  `git push origin gh-pages`
-  `git checkout master`
+  Rake::Task[:render_html].invoke
+  Rake::Task[:push_to_github_pages].invoke
+end
+
+task :push_to_github_pages do
+	puts 'Pushing to github-pages'
+	`git checkout gh-pages`
+	`git merge master`
+	`git push origin gh-pages`
+	`git checkout master`
 end
--- app.rb
+++ app.rb
@@ -7,6 +7,8 @@ require_relative 'data_loader'
 require_relative 'view'
 require_relative 'model'
 
+output_file = "index.html"
+
 person_list = Person_list_with_points.new(raw_data_members, raw_data_feedback)
 
 feedback_matrix_section = print_feedback_matrix_section(person_list)
@@ -14,6 +16,7 @@ leaderboard_section = print_leaderboard_section(person_list)
 
 template = ERB.new(File.read("templates/index.erb"))
 html_content = template.result(binding)
-File.open("index.html", "w") do |file|
+File.open(output_file, "w") do |file|
   file.puts html_content
 end
+puts output_file + ' rendered successfully'
